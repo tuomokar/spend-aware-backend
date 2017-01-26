50380043d72d411f20b4d85ac1fd7a01e1154ffb
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2017-01-26 23:34:41 +0200

    Ditched multiple migration files + some extra data for item
    The migration files aren't really needed for the scope of this project

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -3,14 +3,26 @@
 -- Up
 --------------------------------------------------------------------------------
 
-CREATE TABLE item (
-  name TEXT
+
+CREATE TABLE user (
+  username TEXT UNIQUE NOT NULL CHECK(username != ''),
+  password TEXT NOT NULL CHECK(password != '')
 );
 
-INSERT INTO item (name) VALUES ('testName1');
+INSERT INTO user (username, password) VALUES ('admin1', 'admin1');
+
+CREATE TABLE item (
+  name TEXT NOT NULL CHECK(name != ''),
+  creator INTEGER NOT NULL CHECK (creator > 0),
+  cost REAL NOT NULL CHECK(cost >= 0),
+  date BLOB
+);
+
+INSERT INTO item (name, creator, cost) VALUES ('Milk', 1, 5.0);
 
 --------------------------------------------------------------------------------
 -- Down
 --------------------------------------------------------------------------------
 
+DROP TABLE user;
 DROP TABLE item;
